70d385d2a6c93006f9b6a174a3919e9b857fcd99
ProgerSoul <52043736+ProgerSoul@users.noreply.github.com>
2021-03-12 20:29:07 +0300

    Template
    Template for project Asp Net Core

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EFDataApp.Models; // пространство имен моделей и контекста данных
+using Template.Models;
+
+namespace EFDataApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationContext db;
+        public HomeController(ApplicationContext context)
+        {
+            db = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+        
+    }
+
+    
+}
---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Template.Models;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace EFDataApp.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+    }
+}
